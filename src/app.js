be9b15b418a4f467ae095dcf5b5c378c427f7a61
KevOneRedOne <kevinalves@hotmail.fr>
2024-01-26 10:22:49 +0100

    Update branch

--- src/app.js
+++ src/app.js
@@ -5,33 +5,58 @@ const cors = require('cors');
 const Sequelize = require('sequelize');
 const config = require('./config/config.js')[process.env.NODE_ENV || 'development'];
 const router = require('./routes/index.js');
+const { ApolloServer } = require('@apollo/server');
 
-app.use(cors())
-app.options(process.env.FRONTEND_URL, cors());
+const initApplication = async() => {
+    app.use(cors())
+    app.options(process.env.FRONTEND_URL, cors());
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    port: config.port,
-    host: config.host,
-    dialect: config.dialect,
-    dialectOptions: {
-        connectTimeout: 60000
-    }
-});
-
-sequelize.sync()
-    .then(() => {
-        console.log('database synchronised');
-    })
-    .catch(err => {
-        console.error('database synchronisation error :', err);
+    const sequelize = new Sequelize(config.database, config.username, config.password, {
+        port: config.port,
+        host: config.host,
+        dialect: config.dialect,
+        dialectOptions: {
+            connectTimeout: 60000
+        }
     });
 
-app.get("/", (req, res) => {
-    res.send("Welcome to my API");
-})
+    sequelize.sync()
+        .then(() => {
+            console.log('database synchronised');
+        })
+        .catch(err => {
+            console.error('database synchronisation error :', err);
+        });
 
-app.use("/api", router);
+    app.get("/", (req, res) => {
+        res.send("Welcome to my API");
+    })
 
-app.listen(process.env.PORT, () => {
-    console.log(`server launch on port ${process.env.PORT}`);
-});
+    app.use("/api", router);
+
+    const typeDefs = `
+        type Article {
+            id : ID!
+            title : String
+            description : String
+            
+        }
+    `;
+
+    const serverGraphQl = new ApolloServer({
+        // a passer : les types (typages des entités etdes resolvers)
+        // resolvers: fonctions du CRUD
+            // Querry: GET/READ
+            // Mutation: tout le reste: PUT, DELETE, POST, CREATE, UPDATE
+    })
+
+    await serverGraphQl.start();
+
+    app.use(ExpressMiddleware(ServerGraphQL, {
+        path:'/graphql'
+    }));
+
+    app.listen(process.env.PORT, () => {
+        console.log(`server launch on port ${process.env.PORT}`);
+    });
+}
