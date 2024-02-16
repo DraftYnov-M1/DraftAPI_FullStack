f6a205995fb290df49665798d7e76dbfc3186cfc
Vincent Zerbib <contact@zelabstudio.com>
2024-02-16 10:29:30 +0100

    save work

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -4,16 +4,55 @@ const resolvers = {
     Query: {
         getArticles: async (parent, args, context, info) => {
             const articles = await db.Article.findAll();
+            if (articles.length === 0) {
+                const error = new Error("No articles found");
+                error.extensions.code = "NOT_FOUND";
+                return error;
+            }
             return articles;
         },
         getArticle: (parent, args, context, info) => {
-            console.log(args);
+            const article = db.Article.findByPk(args.id);
+            return article;
+        }
+    },
+    Mutation: {
+        createArticle: async (parent, args, context, info) => { 
+            const { title, description, date } = args.article;
+            const article = await db.Article.create({ title, description, date });
+            return article;
+        },
+        updateArticle: async (parent, args, context, info) => {
+            const { title, description, date } = args.article;
+            const article = await db.Article.findByPk(args.id);
+            if (!article) {
+                const error = new Error("Article not found");
+                error.extensions.code = "NOT_FOUND";
+                return error;
+            }
+            article.title = title;
+            article.description = description;
+            article.date = date;
+            await article.save();
+            return article;
+        },
+        deleteArticle: async (parent, args, content) => {
+            const { id } = args;
+            const article = await db.Article.destroy({
+                where: {
+                    id: id
+                }
+            });
+            if (!article) {
+                return {
+                    message: "Article not found",
+                    success: false
+                }
+            }
             return {
-                id: 1,
-                title: "Article 1",
-                description: "Description de l'article 1",
-                date: "2021-01-01"
-            };
+                message: "Article deleted",
+                success: true
+            }
         }
     }
 }
